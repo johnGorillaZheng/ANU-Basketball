3ce0578fbe292526f491c016d837f44bfa36e2de
Zihao Liu <liuzihaolzh@126.com>
2018-03-26 00:04:46 +1100

    Modify class name

---
+++ source/src/com/pink/anubasketball/bean/RoleInTeam.java
@@ -0,0 +1,31 @@
+package com.pink.anubasketball.bean;
+
+public class RoleInTeam {
+    private int personID;
+    private int TeamID;
+    private String role;
+
+    public int getPersonID() {
+        return personID;
+    }
+
+    public void setPersonID(int personID) {
+        this.personID = personID;
+    }
+
+    public int getTeamID() {
+        return TeamID;
+    }
+
+    public void setTeamID(int teamID) {
+        TeamID = teamID;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
